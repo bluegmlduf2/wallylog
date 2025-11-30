45a2a8192eedfc595e8856acfc0df0404471bf70
wally_yoon <bluegmlduf2@naver.com>
2025-11-30 22:58:43 +0900

    feat: 메일전송화면 css임폴트수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Script from "next/script";
-import "./globals.css";
+import "@/app/globals.css";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
